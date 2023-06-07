adf89f3bd72257943bb80b16c79d45e0c1f539ab
Sunioatm <104454675+Sunioatm@users.noreply.github.com>
2023-06-07 16:16:08 +0700

    Searching room by roomId and name

--- backend/routes/roomRoutes.js
+++ backend/routes/roomRoutes.js
@@ -2,33 +2,98 @@ const express = require("express");
 const router = express.Router();
 const Room = require("../models/roomModel.js");
 
-router.post("/create", async (req, res) => {
-  try {
-    const { name, capacity } = req.body;
-
-    if (!(name && capacity)) {
-      return res.status(400).send("Missing required fields.");
-    }
-
-    const oldRoom = await Room.findOne({ name });
-    if (oldRoom) {
-      return res.status(409).send("That room already exists.");
-    }
-
-    const room = await Room.create({
-      name,
-      capacity,
-    });
-
-    res.status(201).json(room);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Internal server error.");
-  }
-});
-
 router.get("/", (req, res) => {
-  res.send("hi");
+    res.send("hi");
 });
+
+router.post("/create", async (req, res) => {
+    try {
+        const { name, capacity } = req.body;
+
+        if (!(name && capacity)) {
+            return res.status(400).send("Missing required fields.");
+        }
+
+        const oldRoom = await Room.findOne({ name });
+        if (oldRoom) {
+            return res.status(409).send("That room already exists.");
+        }
+
+        const room = await Room.create({
+            name,
+            capacity,
+        });
+
+        res.status(201).json(room);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal server error.");
+    }
+});
+
+router.get("/show", async (req, res) => {
+    try {
+        const rooms = await Room.find();
+        res.status(200).json(rooms);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal server error.");
+    }
+});
+
+router.get("/:roomId", async (req, res) => {
+    try {
+        const roomId = req.params.roomId;
+        const room = await Room.findOne({ roomId });
+
+        if (!room) {
+            return res.status(404).send("Room not found.");
+        }
+
+        res.status(200).json(room);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal server error.");
+    }
+});
+
+router.get("/:name", async (req, res) => {
+    try {
+        const name = req.params.name;
+        const room = await Room.findOne({ name });
+
+        if (!room) {
+            return res.status(404).send("Room not found.");
+        }
+
+        res.status(200).json(room);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal server error: " + error.message);
+    }
+});
+
+router.get("/name/:name", async (req, res) => {
+    try {
+      const name = req.params.name;
+      const temp = await Room.find();
+  
+      const room = temp.find((room) => {
+        room.name.toLowerCase() === name.toLowerCase()
+      })
+  
+      if (!room) {
+        return res.status(404).send("Room not found.");
+      }
+  
+      res.status(200).json(room);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Internal server error: " + error.message);
+    }
+  });
+  
+  
+
 
 module.exports = router;
